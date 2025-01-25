315e7e49ec8b908b4cd76d1eac603bc4b3c4d0a4
strvictor <vicctor1009@gmail.com>
2025-01-25 16:18:07 -0300

    feat: adicionar componentes e estilos iniciais com Tailwind CSS e Radix UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
-    <h1>Ola mundo</h1>
+    <div>
+      <h1>Ola mundo</h1>
+      <Button>aperte-me</Button>
+    </div>
   )
 }
 
