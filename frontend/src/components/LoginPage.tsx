cee39c88b12e5f451695adc8b318bd38734f5397
strvictor <vicctor1009@gmail.com>
2025-01-25 17:56:22 -0300

    feat: atualizar página de login para incluir formulário de cadastro com validação

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -30,28 +30,36 @@ export function LoginPage() {
             </DialogTrigger>
             <DialogContent className="sm:max-w-[625px]">
                 <DialogHeader>
-                <DialogTitle>Edit profile</DialogTitle>
-                <DialogDescription>
-                    Make changes to your profile here. Click save when you're done.
-                </DialogDescription>
+                    <div className="flex flex-col w-auto text-center gap-1.5">
+                        <DialogTitle className="text-2xl">Cadastro</DialogTitle>
+                        <DialogDescription>
+                            Faça seu cadastro aqui. É simples e Seguro!
+                        </DialogDescription>
+                    </div>
                 </DialogHeader>
                 <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                    Name
-                    </Label>
-                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                    <div className="grid grid-cols-4 items-center gap-2">
+                        <Label htmlFor="name">Nome</Label>
+                        <Input id="name" placeholder="Digite seu nome" type="text" className="col-span-4" required/>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-2">
+                        <Label htmlFor="email">E-mail</Label>
+                        <Input id="email" placeholder="Seu melhor e-mail" type="email" className="col-span-4" required/>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-2">
+                        <Label htmlFor="email">Senha:</Label>
+                        <Input id="password" placeholder="Crie uma senha" type="password" className="col-span-4" minLength={8} required/>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-2">
+                        <Label htmlFor="email">Confirme sua senha:</Label>
+                        <Input id="password" placeholder="Confirma a sua senha" type="password" className="col-span-4" minLength={8} required/>
+                    </div>
                 </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                    Username
-                    </Label>
-                    <Input id="username" value="@peduarte" className="col-span-3" />
+                <div className="flex">
+                    <DialogFooter>
+                        <Button type="submit" className="pl-6 pr-6 pt-4 pb-4">Criar Conta</Button>
+                    </DialogFooter>
                 </div>
-                </div>
-                <DialogFooter>
-                <Button type="submit">Save changes</Button>
-                </DialogFooter>
             </DialogContent>
             </Dialog>
         </div>
