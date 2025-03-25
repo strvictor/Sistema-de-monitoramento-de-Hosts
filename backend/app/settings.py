4e8a7149b4f5c636b667139d5b9901f9601bbff6
strvictor <vicctor1009@gmail.com>
2025-03-25 09:33:03 -0300

    feat: remover scripts de deploy e guias de configuração obsoletos, simplificando o processo de instalação e configuração do sistema de monitoramento de hosts

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -132,7 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://34.55.175.64:3000",
-    'http://mnt-hosts.dev-strvictor.online:3000'
+    'http://mnt-hosts.dev-strvictor.online:3000',
+    'https://mnt-hosts.dev-strvictor.online:3000',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
