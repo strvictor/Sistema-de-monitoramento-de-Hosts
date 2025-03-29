a7fbd82b8697e2879b7c2d807fe1e952317225f8
strvictor <vicctor1009@gmail.com>
2025-03-29 19:04:17 -0300

    fix: ajustar configurações de CORS e corrigir URL da API para o ambiente de produção

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -131,9 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://34.55.175.64:3000",
     'http://mnt-hosts.dev-strvictor.online:3000',
+    'http://mnt-hosts.dev-strvictor.online',
     'https://mnt-hosts.dev-strvictor.online:3000',
+    'https://mnt-hosts.dev-strvictor.online',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
